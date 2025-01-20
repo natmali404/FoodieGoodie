bb991c22f99061b222f656106351fabca0b6796d
Natalia Malinowska <58657829+natmali404@users.noreply.github.com>
2025-01-20 15:14:58 +0100

    fix: view routing

--- foodie_goodie/foodie_goodie_app/urls.py
+++ foodie_goodie/foodie_goodie_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
+from .views import main_views
 from .swagger import schema_view
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Home view
+    path('', main_views.home, name='home'),  # Home view
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
