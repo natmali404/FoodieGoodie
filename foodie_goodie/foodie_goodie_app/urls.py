8a7252a5be57e6d0811a656197699cb35e44ddc1
MateuszDejewski <mateusz.dejewski@gmail.com>
2025-01-25 15:45:43 +0100

    addcomment

--- foodie_goodie/foodie_goodie_app/urls.py
+++ foodie_goodie/foodie_goodie_app/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     
     path('przepis/<int:id>/', recipe_views.przepis_detail, name='recipe_detail'),
     path('przepis/<int:id>/stars', recipe_views.przepis_detail_stars, name='recipe_detail_stars'),
+    path('przepis/<int:id>/addcomment', recipe_views.add_komentarz, name='addcomment'),
     
 ]
--- foodie_goodie/foodie_goodie_app/views/recipe_views.py
+++ foodie_goodie/foodie_goodie_app/views/recipe_views.py
@@ -1,10 +1,11 @@
+from datetime import date
 import json
 from django.http import JsonResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from ..models import Przepis,SkladnikPrzepisu,Uzytkownik,OcenyPrzepisu,KomentarzePrzepisu
 from ..recalculatingAmount import recalculate
-
+from ..forms.comment_form import KomentarzForm
 logged_user=Uzytkownik.objects.filter(idUzytkownik=1).first()
 
 def przepis_detail(request, id):
@@ -15,7 +16,7 @@ def przepis_detail(request, id):
     # Pobierz składniki przypisane do przepisu
     skladniki_przepisu = SkladnikPrzepisu.objects.filter(przepis=przepis)
     skladniki=[item.skladnik for item in skladniki_przepisu]
-    komentarza=KomentarzePrzepisu.objects.filter(przepis=przepis)
+    komentarze=KomentarzePrzepisu.objects.filter(przepis=przepis)
     ocena= OcenyPrzepisu.objects.filter(oceniajacy=logged_user).first() 
     if ocena:
         ocena=ocena.wartosc
@@ -24,6 +25,7 @@ def przepis_detail(request, id):
     liczby = range(1, 21)  # Lista liczb od 1 do 20
     porcja=przepis.porcja
     error=""
+    form = KomentarzForm()
     if request.method == 'POST':
         porcja = int(request.POST.get('porcja_select'))
         try:
@@ -35,10 +37,12 @@ def przepis_detail(request, id):
     return render(request, 'recipedetails/recipe_detail.html', {
         'przepis': przepis,
         'skladniki_przepisu': skladniki,
+        'komentarze':komentarze,
         'liczby':liczby,
         'porcja':porcja,
         'error':error,
-        'ocena':ocena
+        'ocena':ocena,
+        'form': form
     })
 
 def przepis_detail_stars(request,id):
@@ -58,6 +62,18 @@ def przepis_detail_stars(request,id):
     return JsonResponse({'message': "Ten adres nie ma metody GET"}, status=400)   
     
 
+def add_komentarz(request, id):
+    przepis = get_object_or_404(Przepis, idPrzepis=id)
+    
+    if request.method == 'POST':
+        form = KomentarzForm(request.POST)
+        if form.is_valid():
+            komentarz = form.save(commit=False)
+            komentarz.przepis = przepis
+            komentarz.uzytkownik = logged_user
+            komentarz.dataKomentarza = date.today()
+            komentarz.save()
 
+    return redirect('recipe_detail', id=przepis.idPrzepis)
     
     
